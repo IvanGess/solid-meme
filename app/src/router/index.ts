6da37fbf0dbfb90d05e55981b9f0fcf361bdf658
IvanGess <ivangess89@gmail.com>
2025-03-04 20:43:54 +0300

    feat: добавить роутинг

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,23 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const LOGIN_PATH = '/login'
+const NOTES_PATH = '/notes'
+
+const routes = [
+  {
+    path: '/',
+    redirect: () => ({ path: NOTES_PATH }),
+  },
+  {
+    path: NOTES_PATH,
+    component: () => {},
+    meta: {
+      requiresAuth: true,
+      title: 'Мои заметки'
+    }
+  },
+  {
+    path: LOGIN_PATH,
+    component: () => {},
+    meta: {
+      requiresGuest: true,
+      title: 'Вход'
+    }
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { top: 0 }
+  }
 })
+
+
 
 export default router
