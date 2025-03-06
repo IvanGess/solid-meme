cd2ca8dc4b421e62753b7e28a872fb8afc5c6e82
IvanGess <ivangess89@gmail.com>
2025-03-06 20:23:55 +0300

    readme: обновить README.md с процессом запуска и ссылкой на проект

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/solid-meme/" : "/",
+
   plugins: [
     vue(),
     vueDevTools(),
